f1c465ea9c934312dc3774028d5f6de1d58503a8
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-12-13 20:20:03 +0100

    Add project files.

---
+++ Designer.Core/Persistence/ProjectStore.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Designer.Domain.Models;
+using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ExtensionModel.Content;
+using ExtendedXmlSerializer.ExtensionModel.Xml;
+
+namespace Designer.Core.Persistence
+{
+    public class ProjectStore : IProjectStore
+    {
+        private readonly IExtendedXmlSerializer serializer;
+
+        public ProjectStore()
+        {
+            serializer = new ConfigurationContainer()
+                .UseAutoFormatting()
+                .UseOptimizedNamespaces()
+                .EnableParameterizedContent()
+                .Register(ColorConverter.Default)
+                .Create();
+        }
+
+        public Task<Project> Load(Stream stream)
+        {
+            var project = serializer.Deserialize<Project>(stream);
+            return Task.FromResult(project);
+        }
+
+        public Task Save(Project project, Stream stream)
+        {
+            serializer.Serialize(stream, project);
+            return Task.CompletedTask;
+        }
+    }
+}
